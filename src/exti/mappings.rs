c8a0e4d2f0ad16059d5412149cf9bad9da06919d
Kai <7957852+kaivol@users.noreply.github.com>
2021-06-24 13:47:55 +0200

    Fixed and reworked EXTI mappings

--- src/exti/mappings.rs
+++ src/exti/mappings.rs
@@ -1,412 +1,244 @@
 use crate::{line::HeadNum, exti_line};
 use drone_stm32_map::periph::{exti::*, gpio::head::*, gpio::pin::*};
 
-macro_rules! head_num {
-    ($($head:ty, $num:expr;)+) => {
-        $(
-            impl HeadNum for $head {
-                const NUM: u32 = $num;
-            }
-        )+
+macro_rules! port {
+    ($head:ident, $num:expr, ($($exti:ident, $pin:ident;)+)) => {
+        impl HeadNum for $head {
+            const NUM: u32 = $num;
+        }
+        $(exti_line!($exti, $head, $pin;);)+
     }
 }
 
-head_num!(
-    GpioAHead, 0;
-    GpioBHead, 1;
-    GpioCHead, 2;
-    GpioDHead, 3;
-    GpioEHead, 4;
-    GpioFHead, 5;
-    GpioGHead, 6;
-    GpioHHead, 7;
-    GpioIHead, 8;
-);
-
-#[cfg(any(
-    stm32_mcu = "stm32f405",
-    stm32_mcu = "stm32f407",
-    stm32_mcu = "stm32f427",
-    stm32_mcu = "stm32f429",
-    stm32_mcu = "stm32f469",
-))]
-head_num!(
-    GpioJHead, 9;
-    GpioKHead, 10;
-);
-
 impl crate::ExtiMap for Exti0 {}
-exti_line!(
-    Exti0, GpioAHead, GpioA0;
-    Exti0, GpioBHead, GpioB0;
-    Exti0, GpioCHead, GpioC0;
-    Exti0, GpioDHead, GpioD0;
-    Exti0, GpioEHead, GpioE0;
-    Exti0, GpioFHead, GpioF0;
-    Exti0, GpioGHead, GpioG0;
-    Exti0, GpioHHead, GpioH0;
-    Exti0, GpioIHead, GpioI0;
-);
-#[cfg(any(
-    stm32_mcu = "stm32f405",
-    stm32_mcu = "stm32f407",
-    stm32_mcu = "stm32f427",
-    stm32_mcu = "stm32f429",
-    stm32_mcu = "stm32f469",
-))]
-exti_line!(
-    Exti0, GpioJHead, GpioJ0;
-    Exti0, GpioKHead, GpioK0;
-);
-
 impl crate::ExtiMap for Exti1 {}
-exti_line!(
-    Exti1, GpioAHead, GpioA1;
-    Exti1, GpioBHead, GpioB1;
-    Exti1, GpioCHead, GpioC1;
-    Exti1, GpioDHead, GpioD1;
-    Exti1, GpioEHead, GpioE1;
-    Exti1, GpioFHead, GpioF1;
-    Exti1, GpioGHead, GpioG1;
-    Exti1, GpioHHead, GpioH1;
-    Exti1, GpioIHead, GpioI1;
-);
-#[cfg(any(
-    stm32_mcu = "stm32f405",
-    stm32_mcu = "stm32f407",
-    stm32_mcu = "stm32f427",
-    stm32_mcu = "stm32f429",
-    stm32_mcu = "stm32f469",
-))]
-exti_line!(
-    Exti1, GpioJHead, GpioJ1;
-    Exti1, GpioKHead, GpioK1;
-);
-
 impl crate::ExtiMap for Exti2 {}
-exti_line!(
-    Exti2, GpioAHead, GpioA2;
-    Exti2, GpioBHead, GpioB2;
-    Exti2, GpioCHead, GpioC2;
-    Exti2, GpioDHead, GpioD2;
-    Exti2, GpioEHead, GpioE2;
-    Exti2, GpioFHead, GpioF2;
-    Exti2, GpioGHead, GpioG2;
-    Exti2, GpioHHead, GpioH2;
-    Exti2, GpioIHead, GpioI2;
-);
-#[cfg(any(
-    stm32_mcu = "stm32f405",
-    stm32_mcu = "stm32f407",
-    stm32_mcu = "stm32f427",
-    stm32_mcu = "stm32f429",
-    stm32_mcu = "stm32f469",
-))]
-exti_line!(
-    Exti2, GpioJHead, GpioJ2;
-    Exti2, GpioKHead, GpioK2;
-);
-
 impl crate::ExtiMap for Exti3 {}
-exti_line!(
-    Exti3, GpioAHead, GpioA3;
-    Exti3, GpioBHead, GpioB3;
-    Exti3, GpioCHead, GpioC3;
-    Exti3, GpioDHead, GpioD3;
-    Exti3, GpioEHead, GpioE3;
-    Exti3, GpioFHead, GpioF3;
-    Exti3, GpioGHead, GpioG3;
-    Exti3, GpioHHead, GpioH3;
-    Exti3, GpioIHead, GpioI3;
-);
-#[cfg(any(
-    stm32_mcu = "stm32f405",
-    stm32_mcu = "stm32f407",
-    stm32_mcu = "stm32f427",
-    stm32_mcu = "stm32f429",
-    stm32_mcu = "stm32f469",
-))]
-exti_line!(
-    Exti3, GpioJHead, GpioJ3;
-    Exti3, GpioKHead, GpioK3;
-);
-
 impl crate::ExtiMap for Exti4 {}
-exti_line!(
-    Exti4, GpioAHead, GpioA4;
-    Exti4, GpioBHead, GpioB4;
-    Exti4, GpioCHead, GpioC4;
-    Exti4, GpioDHead, GpioD4;
-    Exti4, GpioEHead, GpioE4;
-    Exti4, GpioFHead, GpioF4;
-    Exti4, GpioGHead, GpioG4;
-    Exti4, GpioHHead, GpioH4;
-    Exti4, GpioIHead, GpioI4;
-);
-#[cfg(any(
-    stm32_mcu = "stm32f405",
-    stm32_mcu = "stm32f407",
-    stm32_mcu = "stm32f427",
-    stm32_mcu = "stm32f429",
-    stm32_mcu = "stm32f469",
-))]
-exti_line!(
-    Exti4, GpioJHead, GpioJ4;
-    Exti4, GpioKHead, GpioK4;
-);
-
 impl crate::ExtiMap for Exti5 {}
-exti_line!(
-    Exti5, GpioAHead, GpioA5;
-    Exti5, GpioBHead, GpioB5;
-    Exti5, GpioCHead, GpioC5;
-    Exti5, GpioDHead, GpioD5;
-    Exti5, GpioEHead, GpioE5;
-    Exti5, GpioFHead, GpioF5;
-    Exti5, GpioGHead, GpioG5;
-    Exti5, GpioHHead, GpioH5;
-    Exti5, GpioIHead, GpioI5;
-);
-#[cfg(any(
-    stm32_mcu = "stm32f405",
-    stm32_mcu = "stm32f407",
-    stm32_mcu = "stm32f427",
-    stm32_mcu = "stm32f429",
-    stm32_mcu = "stm32f469",
-))]
-exti_line!(
-    Exti5, GpioJHead, GpioJ5;
-    Exti5, GpioKHead, GpioK5;
-);
-
 impl crate::ExtiMap for Exti6 {}
-exti_line!(
-    Exti6, GpioAHead, GpioA6;
-    Exti6, GpioBHead, GpioB6;
-    Exti6, GpioCHead, GpioC6;
-    Exti6, GpioDHead, GpioD6;
-    Exti6, GpioEHead, GpioE6;
-    Exti6, GpioFHead, GpioF6;
-    Exti6, GpioGHead, GpioG6;
-    Exti6, GpioHHead, GpioH6;
-    Exti6, GpioIHead, GpioI6;
-);
-#[cfg(any(
-    stm32_mcu = "stm32f405",
-    stm32_mcu = "stm32f407",
-    stm32_mcu = "stm32f427",
-    stm32_mcu = "stm32f429",
-    stm32_mcu = "stm32f469",
-))]
-exti_line!(
-    Exti6, GpioJHead, GpioJ6;
-    Exti6, GpioKHead, GpioK6;
-);
-
 impl crate::ExtiMap for Exti7 {}
-exti_line!(
-    Exti7, GpioAHead, GpioA7;
-    Exti7, GpioBHead, GpioB7;
-    Exti7, GpioCHead, GpioC7;
-    Exti7, GpioDHead, GpioD7;
-    Exti7, GpioEHead, GpioE7;
-    Exti7, GpioFHead, GpioF7;
-    Exti7, GpioGHead, GpioG7;
-    Exti7, GpioHHead, GpioH7;
-    Exti7, GpioIHead, GpioI7;
-);
-#[cfg(any(
-    stm32_mcu = "stm32f405",
-    stm32_mcu = "stm32f407",
-    stm32_mcu = "stm32f427",
-    stm32_mcu = "stm32f429",
-    stm32_mcu = "stm32f469",
-))]
-exti_line!(
-    Exti7, GpioJHead, GpioJ7;
-    Exti7, GpioKHead, GpioK7;
-);
-
 impl crate::ExtiMap for Exti8 {}
-exti_line!(
-    Exti8, GpioAHead, GpioA8;
-    Exti8, GpioBHead, GpioB8;
-    Exti8, GpioCHead, GpioC8;
-    Exti8, GpioDHead, GpioD8;
-    Exti8, GpioEHead, GpioE8;
-    Exti8, GpioFHead, GpioF8;
-    Exti8, GpioGHead, GpioG8;
-    Exti8, GpioHHead, GpioH8;
-    Exti8, GpioIHead, GpioI8;
-);
-#[cfg(any(
-    stm32_mcu = "stm32f405",
-    stm32_mcu = "stm32f407",
-    stm32_mcu = "stm32f427",
-    stm32_mcu = "stm32f429",
-    stm32_mcu = "stm32f469",
-))]
-exti_line!(
-    Exti8, GpioJHead, GpioJ8;
-    Exti8, GpioKHead, GpioK8;
-);
-
 impl crate::ExtiMap for Exti9 {}
-exti_line!(
-    Exti9, GpioAHead, GpioA9;
-    Exti9, GpioBHead, GpioB9;
-    Exti9, GpioCHead, GpioC9;
-    Exti9, GpioDHead, GpioD9;
-    Exti9, GpioEHead, GpioE9;
-    Exti9, GpioFHead, GpioF9;
-    Exti9, GpioGHead, GpioG9;
-    Exti9, GpioHHead, GpioH9;
-    Exti9, GpioIHead, GpioI9;
-);
-#[cfg(any(
-    stm32_mcu = "stm32f405",
-    stm32_mcu = "stm32f407",
-    stm32_mcu = "stm32f427",
-    stm32_mcu = "stm32f429",
-    stm32_mcu = "stm32f469",
-))]
-exti_line!(
-    Exti9, GpioJHead, GpioJ9;
-    Exti9, GpioKHead, GpioK9;
-);
-
 impl crate::ExtiMap for Exti10 {}
-exti_line!(
-    Exti10, GpioAHead, GpioA10;
-    Exti10, GpioBHead, GpioB10;
-    Exti10, GpioCHead, GpioC10;
-    Exti10, GpioDHead, GpioD10;
-    Exti10, GpioEHead, GpioE10;
-    Exti10, GpioFHead, GpioF10;
-    Exti10, GpioGHead, GpioG10;
-    Exti10, GpioHHead, GpioH10;
-    Exti10, GpioIHead, GpioI10;
-);
-#[cfg(any(
-    stm32_mcu = "stm32f405",
-    stm32_mcu = "stm32f407",
-    stm32_mcu = "stm32f427",
-    stm32_mcu = "stm32f429",
-    stm32_mcu = "stm32f469",
-))]
-exti_line!(
-    Exti10, GpioJHead, GpioJ10;
-    Exti10, GpioKHead, GpioK10;
-);
-
 impl crate::ExtiMap for Exti11 {}
-exti_line!(
-    Exti11, GpioAHead, GpioA11;
-    Exti11, GpioBHead, GpioB11;
-    Exti11, GpioCHead, GpioC11;
-    Exti11, GpioDHead, GpioD11;
-    Exti11, GpioEHead, GpioE11;
-    Exti11, GpioFHead, GpioF11;
-    Exti11, GpioGHead, GpioG11;
-    Exti11, GpioHHead, GpioH11;
-    Exti11, GpioIHead, GpioI11;
-);
-#[cfg(any(
-    stm32_mcu = "stm32f405",
-    stm32_mcu = "stm32f407",
-    stm32_mcu = "stm32f427",
-    stm32_mcu = "stm32f429",
-    stm32_mcu = "stm32f469",
-))]
-exti_line!(
-    Exti11, GpioJHead, GpioJ11;
-    Exti11, GpioKHead, GpioK11;
-);
-
 impl crate::ExtiMap for Exti12 {}
-exti_line!(
-    Exti12, GpioAHead, GpioA12;
-    Exti12, GpioBHead, GpioB12;
-    Exti12, GpioCHead, GpioC12;
-    Exti12, GpioDHead, GpioD12;
-    Exti12, GpioEHead, GpioE12;
-    Exti12, GpioFHead, GpioF12;
-    Exti12, GpioGHead, GpioG12;
-    Exti12, GpioHHead, GpioH12;
-    Exti12, GpioIHead, GpioI12;
-);
-#[cfg(any(
-    stm32_mcu = "stm32f405",
-    stm32_mcu = "stm32f407",
-    stm32_mcu = "stm32f427",
-    stm32_mcu = "stm32f429",
-    stm32_mcu = "stm32f469",
-))]
-exti_line!(
-    Exti12, GpioJHead, GpioJ12;
-    Exti12, GpioKHead, GpioK12;
-);
-
 impl crate::ExtiMap for Exti13 {}
-exti_line!(
-    Exti13, GpioAHead, GpioA13;
-    Exti13, GpioBHead, GpioB13;
-    Exti13, GpioCHead, GpioC13;
-    Exti13, GpioDHead, GpioD13;
-    Exti13, GpioEHead, GpioE13;
-    Exti13, GpioFHead, GpioF13;
-    Exti13, GpioGHead, GpioG13;
-    Exti13, GpioHHead, GpioH13;
-    Exti13, GpioIHead, GpioI13;
-);
-#[cfg(any(
-    stm32_mcu = "stm32f405",
-    stm32_mcu = "stm32f407",
-    stm32_mcu = "stm32f427",
-    stm32_mcu = "stm32f429",
-    stm32_mcu = "stm32f469",
-))]
-exti_line!(
-    Exti13, GpioJHead, GpioJ13;
-    Exti13, GpioKHead, GpioK13;
-);
-
 impl crate::ExtiMap for Exti14 {}
-exti_line!(
-    Exti14, GpioAHead, GpioA14;
-    Exti14, GpioBHead, GpioB14;
-    Exti14, GpioCHead, GpioC14;
-    Exti14, GpioDHead, GpioD14;
-    Exti14, GpioEHead, GpioE14;
-    Exti14, GpioFHead, GpioF14;
-    Exti14, GpioGHead, GpioG14;
-    Exti14, GpioHHead, GpioH14;
-    Exti14, GpioIHead, GpioI14;
-);
-#[cfg(any(
-    stm32_mcu = "stm32f405",
-    stm32_mcu = "stm32f407",
-    stm32_mcu = "stm32f427",
-    stm32_mcu = "stm32f429",
-    stm32_mcu = "stm32f469",
-))]
-exti_line!(
-    Exti14, GpioJHead, GpioJ14;
-    Exti14, GpioKHead, GpioK14;
+impl crate::ExtiMap for Exti15 {}
+
+port!(GpioAHead, 0,
+    (
+        Exti0, GpioA0;
+        Exti1, GpioA1;
+        Exti2, GpioA2;
+        Exti3, GpioA3;
+        Exti4, GpioA4;
+        Exti5, GpioA5;
+        Exti6, GpioA6;
+        Exti7, GpioA7;
+        Exti8, GpioA8;
+        Exti9, GpioA9;
+        Exti10, GpioA10;
+        Exti11, GpioA11;
+        Exti12, GpioA12;
+        Exti13, GpioA13;
+        Exti14, GpioA14;
+        Exti15, GpioA15;
+    )
 );
 
-impl crate::ExtiMap for Exti15 {}
-exti_line!(
-    Exti15, GpioAHead, GpioA15;
-    Exti15, GpioBHead, GpioB15;
-    Exti15, GpioCHead, GpioC15;
-    Exti15, GpioDHead, GpioD15;
-    Exti15, GpioEHead, GpioE15;
-    Exti15, GpioFHead, GpioF15;
-    Exti15, GpioGHead, GpioG15;
-    Exti15, GpioHHead, GpioH15;
-    Exti15, GpioIHead, GpioI15;
+port!(GpioBHead, 1,
+    (
+        Exti0, GpioB0;
+        Exti1, GpioB1;
+        Exti2, GpioB2;
+        Exti3, GpioB3;
+        Exti4, GpioB4;
+        Exti5, GpioB5;
+        Exti6, GpioB6;
+        Exti7, GpioB7;
+        Exti8, GpioB8;
+        Exti9, GpioB9;
+        Exti10, GpioB10;
+        Exti11, GpioB11;
+        Exti12, GpioB12;
+        Exti13, GpioB13;
+        Exti14, GpioB14;
+        Exti15, GpioB15;
+    )
 );
+
+port!(GpioCHead, 2,
+    (
+        Exti0, GpioC0;
+        Exti1, GpioC1;
+        Exti2, GpioC2;
+        Exti3, GpioC3;
+        Exti4, GpioC4;
+        Exti5, GpioC5;
+        Exti6, GpioC6;
+        Exti7, GpioC7;
+        Exti8, GpioC8;
+        Exti9, GpioC9;
+        Exti10, GpioC10;
+        Exti11, GpioC11;
+        Exti12, GpioC12;
+        Exti13, GpioC13;
+        Exti14, GpioC14;
+        Exti15, GpioC15;
+    )
+);
+
+#[cfg(any(
+    stm32_mcu = "stm32f401",
+    stm32_mcu = "stm32f405",
+    stm32_mcu = "stm32f407",
+    stm32_mcu = "stm32f411",
+    stm32_mcu = "stm32f412",
+    stm32_mcu = "stm32f413",
+    stm32_mcu = "stm32f427",
+    stm32_mcu = "stm32f429",
+    stm32_mcu = "stm32f446",
+    stm32_mcu = "stm32f469",
+))]
+port!(GpioDHead, 3,
+    (
+        Exti0, GpioD0;
+        Exti1, GpioD1;
+        Exti2, GpioD2;
+        Exti3, GpioD3;
+        Exti4, GpioD4;
+        Exti5, GpioD5;
+        Exti6, GpioD6;
+        Exti7, GpioD7;
+        Exti8, GpioD8;
+        Exti9, GpioD9;
+        Exti10, GpioD10;
+        Exti11, GpioD11;
+        Exti12, GpioD12;
+        Exti13, GpioD13;
+        Exti14, GpioD14;
+        Exti15, GpioD15;
+    )
+);
+
+#[cfg(any(
+    stm32_mcu = "stm32f401",
+    stm32_mcu = "stm32f405",
+    stm32_mcu = "stm32f407",
+    stm32_mcu = "stm32f411",
+    stm32_mcu = "stm32f412",
+    stm32_mcu = "stm32f413",
+    stm32_mcu = "stm32f427",
+    stm32_mcu = "stm32f429",
+    stm32_mcu = "stm32f446",
+    stm32_mcu = "stm32f469",
+))]
+port!(GpioEHead, 4,
+    (
+        Exti0, GpioE0;
+        Exti1, GpioE1;
+        Exti2, GpioE2;
+        Exti3, GpioE3;
+        Exti4, GpioE4;
+        Exti5, GpioE5;
+        Exti6, GpioE6;
+        Exti7, GpioE7;
+        Exti8, GpioE8;
+        Exti9, GpioE9;
+        Exti10, GpioE10;
+        Exti11, GpioE11;
+        Exti12, GpioE12;
+        Exti13, GpioE13;
+        Exti14, GpioE14;
+        Exti15, GpioE15;
+    )
+);
+
+#[cfg(any(
+    stm32_mcu = "stm32f405",
+    stm32_mcu = "stm32f407",
+    stm32_mcu = "stm32f412",
+    stm32_mcu = "stm32f413",
+    stm32_mcu = "stm32f427",
+    stm32_mcu = "stm32f429",
+    stm32_mcu = "stm32f446",
+    stm32_mcu = "stm32f469",
+))]
+port!(GpioFHead, 5,
+    (
+        Exti0, GpioF0;
+        Exti1, GpioF1;
+        Exti2, GpioF2;
+        Exti3, GpioF3;
+        Exti4, GpioF4;
+        Exti5, GpioF5;
+        Exti6, GpioF6;
+        Exti7, GpioF7;
+        Exti8, GpioF8;
+        Exti9, GpioF9;
+        Exti10, GpioF10;
+        Exti11, GpioF11;
+        Exti12, GpioF12;
+        Exti13, GpioF13;
+        Exti14, GpioF14;
+        Exti15, GpioF15;
+    )
+);
+
+#[cfg(any(
+    stm32_mcu = "stm32f405",
+    stm32_mcu = "stm32f407",
+    stm32_mcu = "stm32f412",
+    stm32_mcu = "stm32f413",
+    stm32_mcu = "stm32f427",
+    stm32_mcu = "stm32f429",
+    stm32_mcu = "stm32f446",
+    stm32_mcu = "stm32f469",
+))]
+port!(GpioGHead, 6,
+    (
+        Exti0, GpioG0;
+        Exti1, GpioG1;
+        Exti2, GpioG2;
+        Exti3, GpioG3;
+        Exti4, GpioG4;
+        Exti5, GpioG5;
+        Exti6, GpioG6;
+        Exti7, GpioG7;
+        Exti8, GpioG8;
+        Exti9, GpioG9;
+        Exti10, GpioG10;
+        Exti11, GpioG11;
+        Exti12, GpioG12;
+        Exti13, GpioG13;
+        Exti14, GpioG14;
+        Exti15, GpioG15;
+    )
+);
+
+port!(GpioHHead, 7,
+    (
+        Exti0, GpioH0;
+        Exti1, GpioH1;
+        Exti2, GpioH2;
+        Exti3, GpioH3;
+        Exti4, GpioH4;
+        Exti5, GpioH5;
+        Exti6, GpioH6;
+        Exti7, GpioH7;
+        Exti8, GpioH8;
+        Exti9, GpioH9;
+        Exti10, GpioH10;
+        Exti11, GpioH11;
+        Exti12, GpioH12;
+        Exti13, GpioH13;
+        Exti14, GpioH14;
+        Exti15, GpioH15;
+    )
+);
+
 #[cfg(any(
     stm32_mcu = "stm32f405",
     stm32_mcu = "stm32f407",
@@ -414,7 +246,79 @@ exti_line!(
     stm32_mcu = "stm32f429",
     stm32_mcu = "stm32f469",
 ))]
-exti_line!(
-    Exti15, GpioJHead, GpioJ15;
-    Exti15, GpioKHead, GpioK15;
+port!(GpioIHead, 8,
+    (
+        Exti0, GpioI0;
+        Exti1, GpioI1;
+        Exti2, GpioI2;
+        Exti3, GpioI3;
+        Exti4, GpioI4;
+        Exti5, GpioI5;
+        Exti6, GpioI6;
+        Exti7, GpioI7;
+        Exti8, GpioI8;
+        Exti9, GpioI9;
+        Exti10, GpioI10;
+        Exti11, GpioI11;
+        Exti12, GpioI12;
+        Exti13, GpioI13;
+        Exti14, GpioI14;
+        Exti15, GpioI15;
+    )
+);
+
+#[cfg(any(
+    stm32_mcu = "stm32f405",
+    stm32_mcu = "stm32f407",
+    stm32_mcu = "stm32f427",
+    stm32_mcu = "stm32f429",
+    stm32_mcu = "stm32f469",
+))]
+port!(GpioJHead, 9,
+    (
+        Exti0, GpioJ0;
+        Exti1, GpioJ1;
+        Exti2, GpioJ2;
+        Exti3, GpioJ3;
+        Exti4, GpioJ4;
+        Exti5, GpioJ5;
+        Exti6, GpioJ6;
+        Exti7, GpioJ7;
+        Exti8, GpioJ8;
+        Exti9, GpioJ9;
+        Exti10, GpioJ10;
+        Exti11, GpioJ11;
+        Exti12, GpioJ12;
+        Exti13, GpioJ13;
+        Exti14, GpioJ14;
+        Exti15, GpioJ15;
+    )
+);
+
+#[cfg(any(
+    stm32_mcu = "stm32f405",
+    stm32_mcu = "stm32f407",
+    stm32_mcu = "stm32f427",
+    stm32_mcu = "stm32f429",
+    stm32_mcu = "stm32f469",
+))]
+port!(GpioKHead, 10,
+    (
+        Exti0, GpioK0;
+        Exti1, GpioK1;
+        Exti2, GpioK2;
+        Exti3, GpioK3;
+        Exti4, GpioK4;
+        Exti5, GpioK5;
+        Exti6, GpioK6;
+        Exti7, GpioK7;
+        Exti8, GpioK8;
+        Exti9, GpioK9;
+        Exti10, GpioK10;
+        Exti11, GpioK11;
+        Exti12, GpioK12;
+        Exti13, GpioK13;
+        Exti14, GpioK14;
+        Exti15, GpioK15;
+    )
 );
